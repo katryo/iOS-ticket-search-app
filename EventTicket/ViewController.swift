f75cfd9b3940299faa0c6b6852752c6ba9057ac0
katryo <katoryo55@gmail.com>
2018-11-15 17:45:07 -0800

    Initial Commit

---
+++ EventTicket/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EventTicket
+//
+//  Created by RYOKATO on 2018/11/15.
+//  Copyright © 2018 Denkinovel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
