39a63c2c6af223eb91c50a0e7f0be0414f1f1311
katryo <katoryo55@gmail.com>
2018-11-19 21:21:22 -0800

    can synchronize category picker and textfield

--- EventTicket/SearchViewController.swift
+++ EventTicket/SearchViewController.swift
@@ -9,14 +9,56 @@
 import UIKit
 import SwiftSpinner
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    
+    let units = ["miles" ,"kms"]
+    let categories = ["Music", "Sports"]
+    let categoryPicker = UIPickerView()
+    @IBOutlet weak var keywordField: UITextField!
+    @IBOutlet weak var categoryField: UITextField!
+    @IBOutlet weak var unitPicker: UIPickerView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.categoryField.inputView = categoryPicker
+        categoryPicker.delegate = self
+        categoryPicker.dataSource = self
+        
+        unitPicker.delegate = self
+        unitPicker.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView == categoryPicker {
+            return self.categories.count
+        } else {
+            return self.units.count
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView == categoryPicker {
+            return categories[row]
+        } else {
+            return units[row]
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if pickerView == categoryPicker {
+            categoryField.text = categories[row]
+        } else {
+            print("On no")
+        }
     }
 
     @IBAction func searchButtonPushed(_ sender: UIButton) {
+        print(self.keywordField.text!)
         
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let vc = storyboard.instantiateViewController(withIdentifier: "EventsTableView") as! EventsTableViewController
