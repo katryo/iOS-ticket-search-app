4f5b85d64741460d879995f82133504b42905101
katryo <katoryo55@gmail.com>
2018-11-23 19:27:55 -0800

    can switch views

---
+++ EventTicket/SegmentedViewController.swift
@@ -0,0 +1,68 @@
+//
+//  SegmentedViewController.swift
+//  EventTicket
+//
+//  Created by RYOKATO on 2018/11/23.
+//  Copyright © 2018 Denkinovel. All rights reserved.
+//
+
+import UIKit
+
+class SegmentedViewController: UIViewController {
+
+    @IBOutlet weak var viewContainer: UIView!
+    lazy var searchVC = storyboard!.instantiateViewController(withIdentifier: "SearchView") as! SearchViewController
+    lazy var favoriteVC = storyboard!.instantiateViewController(withIdentifier: "FavoriteEventsViewController")
+    var currentVC: UIViewController?
+    
+    //var views: [UIView]!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        currentVC = searchVC
+        showCurrentView(0)
+//        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//        let vc = storyboard.instantiateViewController(withIdentifier: "SearchView") as! SearchViewController
+//        self.addChild(vc)
+        // Do any additional setup after loading the view.
+//    viewContainer!.addSubview(vc.view)
+//        viewContainer!.bringSubviewToFront(vc.view)
+    }
+    @IBAction func segmentChanged(_ sender: UISegmentedControl) {
+        self.currentVC!.view.removeFromSuperview()
+        self.currentVC!.removeFromParent()
+        showCurrentView(sender.selectedSegmentIndex)
+    }
+    
+    func viewController(with index: Int) -> UIViewController? {
+        if index == 0 {
+            return searchVC
+        } else {
+            return favoriteVC
+        }
+    }
+    
+    func showCurrentView(_ idx: Int){
+        if let vc = viewController(with: idx) {
+            
+            self.addChild(vc)
+            vc.didMove(toParent: self)
+            
+//            vc.view.frame = self.contentView.bounds
+            self.viewContainer.addSubview(vc.view)
+            self.currentVC = vc
+        }
+    }
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
